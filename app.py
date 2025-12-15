e85d298b8851f2de741028aebc20f9ae08e251f1
Your Name <you@example.com>
2025-12-15 12:13:17 +0100

    feat: CI/CD test final V4

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return '<h1>Bienvenue sur mon application Python conteneurisée sur GKE ceci est la V2 !</h1>'
+    return 'Bienvenue sur mon application Python conteneurisée sur GKE ceci est la V2 !'
 
 if __name__ == '__main__':
     # Écoute sur toutes les interfaces (nécessaire pour Docker)
